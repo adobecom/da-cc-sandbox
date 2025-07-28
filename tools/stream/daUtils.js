f56a9c9301e465982b4c382b4730122e82557210
Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
2025-07-28 12:57:05 +0530

    [POC] Added command-center

--- tools/stream/daUtils.js
+++ tools/stream/daUtils.js
@@ -4,10 +4,16 @@ import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 (async () => {
   try {
     const { context, token } = await DA_SDK;
-    const { repo, path } = context;
-    window.location.replace(
-      `https://enigma--cc--aishwaryamathuria.aem.live/enigma/index.html`
-    );
+    const { repo, path, ref } = context;
+    let appPath =
+      'https://enigma--cc--aishwaryamathuria.aem.live/enigma/index.html';
+    switch (ref) {
+      case 'command-center':
+        appPath =
+          'https://poc-command-center--cc--adobecom.aem.live/tools/command-center/index.html';
+        break;
+    }
+    window.location.replace(appPath);
   } catch (error) {
     console.error('Error initializing DA_SDK:', error);
   }
