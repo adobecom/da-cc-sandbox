a5900bc5bcb9d0c04b7070d59c247eb3733b30cb
Aishwarya Mathuria <mathuria@adobe.com>
2026-01-07 17:37:02 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('fetch', (event) => {
+  console.log('worker invoked')
+  const modifiedRequest = new Request(event.request, {
+    headers: new Headers({
+      ...event.request.headers,
+      'X-Custom-Header': 'customHeader'
+    }),
+    mode: 'cors'
+  });
+  event.respondWith(fetch(modifiedRequest));
+});
