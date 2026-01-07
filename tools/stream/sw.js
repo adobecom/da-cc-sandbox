ee0aea6c237d6965631e2ffbfe3e02139fb4ae71
Aishwarya Mathuria <mathuria@adobe.com>
2026-01-07 17:00:47 +0530

    Implement fetch event listener in sw.js

---
+++ tools/stream/sw.js
@@ -0,0 +1,9 @@
+self.addeventListener('fetch', (event) => {
+  const modifiedRequest = new Request(event.request, {
+    headers: new Headers({
+      ...event.request.headers,
+      'X-Custom-Header': 'customHeader'
+    })
+    mode: 'cors'
+  })
+})
