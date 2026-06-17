26bd765584a634fd38e7d35bf3d4ca6ace89a0df
mathuria <mathuria@adobe.com>
2026-06-17 18:39:17 +0530

    Adding script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -62,6 +62,7 @@ const CONFIG = {
   // imsScope: 'AdobeID,openid,gnav',
   // geoRouting: 'off',
   // fallbackRouting: 'off',
+  imsClientId: 'adobedotcom-cc',
   decorateArea,
   locales: {
     '': { ietf: 'en-US', tk: 'hah7vzn.css' },
